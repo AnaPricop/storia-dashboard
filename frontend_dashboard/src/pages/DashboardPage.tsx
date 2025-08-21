63a305f26a3790ee965fa1907925bfa683ea7cff
AnaPricop <pricop.ana17@gmail.com>
2025-08-21 19:46:48 +0300

    api_base_url from localhost to vite_api

--- frontend_dashboard/src/pages/DashboardPage.tsx
+++ frontend_dashboard/src/pages/DashboardPage.tsx
@@ -23,8 +23,8 @@ ChartJS.register(
     Tooltip,
     Legend
 );
-//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//const API_BASE_URL = 'http://localhost:3001';
 
 interface DistrictStat {
     DISTRICT: string;
--- frontend_dashboard/src/pages/ListingPage.tsx
+++ frontend_dashboard/src/pages/ListingPage.tsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 import Filters from '../components/Filters';
 import { useFilters } from '../contexts/FiltersContext';
 import qs from 'qs';
-//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const API_BASE_URL = 'http://localhost:3001';
 import ClipLoader from "react-spinners/ClipLoader";
 interface Listing {
     TITLU: string;
