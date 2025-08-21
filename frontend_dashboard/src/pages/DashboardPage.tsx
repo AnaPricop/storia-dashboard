79c74cd8721dae977e603c704ae868775fcd5032
AnaPricop <pricop.ana17@gmail.com>
2025-08-21 17:47:27 +0300

    Deploy Vercel

--- frontend_dashboard/src/pages/DashboardPage.tsx
+++ frontend_dashboard/src/pages/DashboardPage.tsx
@@ -23,7 +23,7 @@ ChartJS.register(
     Tooltip,
     Legend
 );
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'import.meta.env.VITE_API_BASE_URL';
 
 interface DistrictStat {
     DISTRICT: string;
